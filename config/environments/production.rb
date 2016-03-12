af47577a64c8685f4d84620c0f10effb2d0c7f0e
Sudipta Mondal <sudiptamondal05@gmail.com>
2016-03-12 15:59:20 +0530

    email fix 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Ecosr::Application.configure do
+  require 'tlsmail'
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
